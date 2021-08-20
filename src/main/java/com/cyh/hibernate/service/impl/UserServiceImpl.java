7c868c0de6cb4461036c8e70ea1d891b31265cd9
cyh-ai <cyh970511@163.com>
2021-08-20 14:10:04 +0800

    程亚辉第一次上传

---
+++ src/main/java/com/cyh/hibernate/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.cyh.hibernate.service.impl;
+
+import com.cyh.hibernate.Utils.Result;
+import com.cyh.hibernate.dao.UserRepository;
+import com.cyh.hibernate.entity.User;
+import com.cyh.hibernate.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author 程亚辉
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    UserRepository userRepository;
+
+    @Override
+    public Result saveUser(User user) {
+        User save = userRepository.save(user);
+        return new Result(save);
+    }
+
+    @Override
+    public Result queryAll() {
+        List<User> users = userRepository.findAll();
+        return new Result(users);
+    }
+
+    @Override
+    public Result getByNameAndAge(String name, int age) {
+        List<User> byNameAndAge = userRepository.getByNameAndAge(name, age);
+        return new Result(byNameAndAge);
+    }
+}
